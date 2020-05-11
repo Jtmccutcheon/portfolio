2a085e4df0505dcbc8d2344676fa6812dc5b611b
unknown <jt.mccutcheon@live.com>
2020-05-11 15:33:59 -0400

    added card flips on projects

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,107 +1,128 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 //images
-import apolloimg from "../assets/apolloimg";
-import css from "../assets/css.png";
-import GitHub from "../assets/GitHub.png";
-import html5s from "../assets/html5s.png";
-import jsImg from "../assets/js.png";
-import reactimg from "../assets/reactimg.png";
-import resticon from "../assets/resticon.png";
-import nodeIcon from "../assets/node-icon.png";
-import graphql from "../assets/graphql.png";
-import sql3 from "../assets/sql3.png";
-import heroku from "../assets/heroku.png";
+import apolloimg from '../assets/apolloimg.png';
+import css from '../assets/css.png';
+import GitHub from '../assets/GitHub.png';
+import html5s from '../assets/html5s.png';
+import jsImg from '../assets/js.png';
+import reactimg from '../assets/reactimg.png';
+import resticon from '../assets/resticon.png';
+import nodeIcon from '../assets/node-icon.png';
+import graphql from '../assets/graphql.png';
+import sql3 from '../assets/sql3.png';
+import heroku from '../assets/heroku.png';
+import python from '../assets/python.png';
+import mongo from '../assets/mongo.png';
+import sass from '../assets/sass.png';
 
-const Skills = props => {
+const Skills = (props) => {
 	// card objects
 	let skillCards = [
 		{
-			title: "Html5",
+			title: 'Html5',
 			img: html5s,
 			description:
-				"Experince using html to seperate web elements into appropriate html tags and display data",
+				'Experince using html to seperate web elements into appropriate html tags and display data',
 		},
 		{
-			title: "CSS",
+			title: 'CSS',
 			img: css,
 			description:
-				"Experience in using css to make UI with and without a UX designer. And Also have experience with the Sass and Less frameworks",
+				'Experience in using css to make UI with and without a UX designer',
 		},
 		{
-			title: "Javascript",
+			title: 'Sass',
+			img: sass,
+			description:
+				'Experience using the sass css framework to handle scalable styling for big apps',
+		},
+		{
+			title: 'Javascript',
 			img: jsImg,
 			description:
-				"Experince writing javascript es6, DOM manipulation, writing functions, bulit in methods,",
+				'Experince writing javascript es6, DOM manipulation, writing functions, bulit in methods,',
 		},
 		{
-			title: "React",
+			title: 'Python',
+			img: python,
+			description:
+				'Learned computer science fundamentals with hands on projects using python, as well as experience in Django and Flask',
+		},
+		{
+			title: 'React',
 			img: reactimg,
 			description:
-				"The bulk of my experience on building front end is in the React framework utilzing popular libraies",
+				'The bulk of my experience on building front end is in the React framework utilzing popular libraies',
 		},
 		{
-			title: "Node",
+			title: 'Node',
 			img: nodeIcon,
 			description:
-				"Experience building node servers withe express framwork",
+				'Experience building node servers withe express framwork',
 		},
 		{
-			title: "REStful apis",
+			title: 'REStful apis',
 			img: resticon,
 			description:
-				"Experince in working with and building restful api endpoints to build full crud functionality",
+				'Experince in working with and building restful api endpoints to build full crud functionality',
 		},
 		{
-			title: "Apollo",
+			title: 'Apollo',
 			img: apolloimg,
 			description:
-				"Experience with Apollo servers using apollo gateway/federation on back end and apollo client store on the front end",
+				'Experience with Apollo servers using apollo gateway/federation on back end and apollo client store on the front end',
 		},
 		{
-			title: "Github",
+			title: 'Github',
 			img: GitHub,
 			description:
-				"Experience working with online cloud version control utilizing seperate production, staging, development, feature branches",
+				'Experience working with online cloud version control utilizing seperate production, staging, development, feature branches',
 		},
 		{
-			title: "Graphql",
+			title: 'Graphql',
 			img: graphql,
 			description:
-				"Experience building graphql apis writing resovers, queries, mutations",
+				'Experience building graphql apis writing resovers, queries, mutations',
 		},
 		{
-			title: "Sqlite 3",
+			title: 'Sql',
 			img: sql3,
 			description:
-				"Experience using sqlite3 databases tables, queries, joins",
+				'Experience using sqlite3 databases tables, queries, joins',
 		},
 		{
-			title: "Heroku",
+			title: 'MongoDB',
+			img: mongo,
+			description:
+				'Experience using mongo to create a noSql database and rest api using mongoose',
+		},
+		{
+			title: 'Heroku',
 			img: heroku,
 			description:
-				"Experince with live deployment/configuration using heroku linked with a github repository master branch",
+				'Experince with live deployment/configuration using heroku linked with a github repository master branch',
 		},
 	];
 
 	// inline style line 105
 	const imgSize = {
-		width: "64px",
+		width: '64px',
 	};
 
 	// returning the header as a function to keep it above  the skill cards
 	const skillHeader = () => {
 		return (
 			<div className='skill-header'>
-				<h2>Proficient with these technologies</h2>
+				<h2>Proficient in these technologies</h2>
 			</div>
 		);
 	};
 
 	// function to map out the skill card object
 	const skillFunc = () => {
-		return skillCards.map(card => {
+		return skillCards.map((card) => {
 			return (
 				<div className='cards'>
 					<img src={card.img} alt='technologies' style={imgSize} />
